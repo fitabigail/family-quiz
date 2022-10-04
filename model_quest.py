6703cfe76e2693825adea5a7c46cc4354e48f93f
fitabigail <abidinu35@gmail.com>
2022-10-04 10:00:43 +0000

    Initial commit add question class to model_quest.py and add question data to quiz_question.py

---
+++ model_quest.py
@@ -0,0 +1,13 @@
+class QuizQueston:
+    """
+    this create  a class for question model with two attributes text and anwer
+    """
+    def __init__(self, quiz_text, quiz_answer):
+        self.text = quiz_text
+        self.answer = quiz_answer
+
+"""
+new_q = QuizQueston("ASSS", "FALSE")
+print(new_q.text) 
+  
+"""     
---
+++ quiz_questions.py
@@ -0,0 +1,22 @@
+quiz_question_data = [
+    
+    {"text": "Who is the Special in the Lego Movie?",
+     "answer": "Emmet Brickowski"},
+    {"text": "What colour are most buses in London?",
+     "answer": "Red"},
+    {"text": "What's the name of the talking snowman in Disney's Frozen?",
+     "answer": "Olaf"},
+    {"text": "What do the wheel's on the bus do in the song?",
+     "answer": "Go round and round"},
+    {"text": "What is taller, an elephant or a giraffe?",
+     "answer": "A giraffe!"},
+    {"text": "Who are Harry Potter's two best friends?",
+     "answer": "Ron Weasley and Hermione Granger"},
+    {"text": "What is the name of the cowboy in Toy Story?",
+     "answer": "Woody"},
+    {"text": "How many planets are in our solar system?",
+     "answer": "Eight"},
+    {"text": "How many players are in a football team?",
+     "answer": "11 players"},
+    {"text": "How many zeros are there in one thousand?", "answer": "Three"}
+]
